3c4ccfcd6f67e228a26e33190d8c1b1e0c3a44eb
caio <caiosantos.costa02@gmail.com>
2024-02-07 16:47:56 -0300

    update

---
+++ life-cycle/src/app/title/title.component.ts
@@ -0,0 +1,29 @@
+import {
+	Component,
+	Input,
+	OnChanges,
+	OnInit,
+	SimpleChanges,
+} from '@angular/core';
+
+@Component({
+	selector: 'app-title',
+	templateUrl: './title.component.html',
+	styleUrls: ['./title.component.css'],
+})
+export class TitleComponent implements OnInit, OnChanges {
+	@Input() nome: string = '';
+
+	constructor() {
+		console.log(`Construtor ${this.nome}`);
+	}
+	
+	ngOnChanges(): void {
+		console.log(`OnChange ${this.nome}`);
+	}
+	
+	ngOnInit(): void {
+		console.log(`OnOnit ${this.nome}`);
+	}
+	
+}
